022110a2e07af0347aeaadb02cbb6b8bc1d8d48a
MatheusAltino <matheusaltino.dev@gmail.com>
2022-05-15 19:18:25 -0300

    :sparkles: Add Project entity

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "../app/entity/User"
+import { Project } from "../app/entity/Project"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -11,7 +12,7 @@ export const AppDataSource = new DataSource({
     database: "devplace-db",
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [User, Project],
     migrations: [],
     subscribers: [],
 })
